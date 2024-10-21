6b5af9c5d11f6e4a1fcb24efc37592b6ef332dab
crom <crom.kim@juice.co.kr>
2024-10-21 23:54:34 +0900

    Revert "vite.config.js변경"
    This reverts commit 114cdf49e383ac3db4020621a17f20cb95ce5827.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/owl/',
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 4200,
+    proxy: {
+      '/api': {
+        target: 'http://3.36.51.130:8000', // 실제 API 서버 URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: true,
+      },
+    },
+  },
+  resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
+});
