dce79e3aa5763f0acbfe3b6b914131b93ba04e03
crom <crom.kim@juice.co.kr>
2024-10-22 14:37:11 +0900

    fix target

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 4200,
     proxy: {
       '/api': {
-        target: 'http://3.36.51.130:8000', // 실제 API 서버 URL
+        target: 'https://api.owluda.com', // 실제 API 서버 URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: true,
